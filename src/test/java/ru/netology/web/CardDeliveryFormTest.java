b8a4d28f98299fd6c022fe9b84cc2e623c3a5c87
Alexandr Shchelokov <alex175171@gmail.com>
2024-05-09 19:22:05 +0300

    Task 1 completed

---
+++ src/test/java/ru/netology/web/CardDeliveryFormTest.java
@@ -0,0 +1,33 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.FaasDocumentOperationValues.INSERT;
+import static java.time.LocalDate.now;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+import static org.openqa.selenium.Keys.LEFT_CONTROL;
+
+public class CardDeliveryFormTest {
+
+    @Test
+    public void shouldTestForm() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").sendKeys(LEFT_CONTROL, INSERT, BACK_SPACE);
+        $("[data-test-id=date] input").setValue((now().plusDays( 3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))));
+        $("[data-test-id=name] input").setValue("Александр");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+
+    }
+
+
+}
